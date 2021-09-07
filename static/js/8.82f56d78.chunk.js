00f1d7daf726b2dc707e56ed1280fa872d7c3974
Kurylenko Serhii <sergey25111988@gmail.com>
2021-09-07 15:02:35 +0300

    Updates

---
+++ static/js/8.82f56d78.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[8],{270:function(t,e,n){"use strict";n.r(e);var c=n(9);var i=Object(c.a)("div",{target:"e1dmvk5i0"})({name:"vhsww9",styles:"width:100%;height:calc(100vh - 80px);display:flex;align-items:center;justify-content:center"}),s=n(2);e.default=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)(i,{})})}}}]);
+//# sourceMappingURL=8.82f56d78.chunk.js.map
